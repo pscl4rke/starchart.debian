13e4002cdf9eb4fd429476b05b342971ed3fac0d
P. S. Clarke <debs@pscl4rke.net>
2015-01-14 10:48:14 +0000

    Some tidying to quieten lintian.

--- menu.ex
+++
@@ -1,2 +0,0 @@
-?package(starchart):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="starchart" command="/usr/bin/starchart"
--- starchart.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the starchart package
-#
-0 4	* * *	root	[ -x /usr/bin/starchart_maintenance ] && /usr/bin/starchart_maintenance
